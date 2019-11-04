ba00fb614ade5e66902402c3e296ccbf7bee2751
Nicolas Combe <combe98@gmail.com>
2019-11-03 22:26:13 -0500

    Add webView

---
+++ PokeSwiftUI/PokeSwiftUI/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  PokeSwiftUI
+//
+//  Created by nicolasCombe on 11/3/19.
+//  Copyright © 2019 Nicolas Combe. All rights reserved.
+//
+
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let safeString = urlString else { return }
+        if let url = URL(string: safeString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+    
+}
