15c582b20aef577a19ada68b94e5744470441001
Nicolas Combe <combe98@gmail.com>
2019-11-03 22:02:12 -0500

    Create project

---
+++ PokeSwiftUI/PokeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeSwiftUI
+//
+//  Created by nicolasCombe on 11/3/19.
+//  Copyright © 2019 Nicolas Combe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
